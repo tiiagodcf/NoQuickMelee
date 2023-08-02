ef890f513d3728c1ca00fc80a71f6c1a55ee5353
Bobby <103152798+tiiagodcf@users.noreply.github.com>
2023-08-02 18:52:27 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,9 +1,15 @@
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(100)
-        local ped = PlayerPedId()
-        if IsPedRunning(ped) then
-            DisableControlAction(0, 140, true) -- Disable the melee attack while running
+        Citizen.Wait(0) -- Always include a wait in a while true loop to prevent freezing
+
+        local playerPed = PlayerPedId()
+
+        if IsPedSprinting(playerPed) then
+            DisableControlAction(0, 24, true) -- disable attack
+            DisableControlAction(0, 257, true) -- disable attack 2
+            DisableControlAction(0, 25, true) -- disable aim
+            DisableControlAction(0, 263, true) -- disable melee
+            DisableControlAction(0, 264, true) -- disable melee 2
         end
     end
 end)
